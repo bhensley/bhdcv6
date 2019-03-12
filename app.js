c21144ea89a66f82e5f1baa2ec82c5d91a1734cd
Bob Hensley <bob@bobbyhensley.com>
2019-03-12 23:01:04 +0000

    Minor modifications made to layout.

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cacheControl = require('express-cache-control');
+//var fs = require('fs');
+//var https = require('https');
 
 var app = express();
+var cache = new cacheControl().middleware;
+
+/*
+// SSL
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/bobbyhensley.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/bobbyhensley.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/bobbyhensley.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Use 80 in production
+//app.set('port',80)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,7 +38,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res, next) {
+app.get('/', cache("years", 1), function(req, res, next) {
+  res.set({ 'Cache-Control': 'public, max-age=31536000'});
+  //res.setHeader('Cache-Control', 'public, max-age=31536000');
   res.render('layout', { title: 'Bobby Hensley dot Com' });
 });
 
